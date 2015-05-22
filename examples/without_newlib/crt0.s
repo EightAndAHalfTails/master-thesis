eba6452e8ea878adfd2c91151b5c13cf36d6443e
Jake Humphrey <jhumphrey@a405-10.enst.fr>
2015-05-22 14:25:57 +0200

    replaced oversized immediates

--- examples/without_newlib/crt0.s
+++ examples/without_newlib/crt0.s
@@ -11,30 +11,34 @@ dpgflt: # add pte
 	.org 0x900
 	
 dtlbms:	# find pte and add to tlb
-	l.mfspr r13, r0, 0x20 		# exception pc reg -> r13
 	l.mfspr r15, r0, 0x30 		# exception ea reg -> r15
 	l.ori	r17, r0, 0x2000		# table base pointer -> r17
 	
-	l.andi	r19, r15, 0xfff		# mask upper bytes of ea
+	l.andi	r19, r15, 0x0fff	# mask upper bytes of ea
 	l.add	r19, r19, r17		# offset into page table
 	l.lwz	r19, 0(r15)		# page table entry -> r19
 
-	l.andi	r21, r15, 0x3f000	# extract bits 18-13 of ea
+	l.movhi	r13, 0x0003
+	l.ori	r13, r13, 0xf000
+	l.and	r21, r15, r13		# extract bits 18-13 of ea
 	l.srli	r21, r21, 12		# r21 <- tlb reg num
 
 	# dtlbw0mrX[31:12] <- ea[31:19]
-	l.andi	r23, r15, 0xfff00000	# extract bits 31:19 of ea
+	l.movhi	r13, 0xfff0
+	l.and	r23, r15, r13		# extract bits 31:19 of ea
 	l.srli	r23, r23, 7		# move to vpn position
 	l.ori	r23, r23, 1		# set valid bit
 	l.mtspr	r21, r23, 0x0a00	# move to tlb match reg
 
 	# dtlbw0trX[31:12] <- pte[31:10]
-	l.andi	r23, r15, 0xfffffb00	# extract bits 31:10 of pte
+	l.movhi	r13, 0xffff
+	l.ori	r13, r13, 0xfb00
+	l.and	r23, r15, r13		# extract bits 31:10 of pte
 	l.slli	r23, r23, 2		# move to ppn location
-	l.andi	r25, r15, 0x3f		# extract bits 5:0 of pte
+	l.andi	r25, r15, 0x003f	# extract bits 5:0 of pte
 	l.or	r23, r23, r15		# add to translate reg
-	l.nop # look up protection bits
-	l.ori	r23, r23, 0x3b0		# add protection bits to reg
+	#l.nop # look up protection bits
+	l.ori	r23, r23, 0x03b0	# add protection bits to reg
 	l.mtspr	r21, r23, 0x0a80	# move to tlb translate reg
 
 	# dtlbwYtrX:
@@ -62,9 +66,11 @@ _start:
 	l.or	r2, r0, r1
 
 	# add base pointer to tlb
-	l.ori	r13, r0, 0x02000001 	# vpn = 0x2000
+	l.movhi r13, 0x0200
+	l.ori	r13, r13, 0x0001 	# vpn = 0x2000
 	l.mtspr	r0, r13, 0x0a00		#  put in w0mr0
-	l.ori	r13, r0, 0x080003b1 	# ppn = 0x2000
+	l.movhi	r13, 0x0800
+	l.ori	r13, r13, 0x03b1 	# ppn = 0x2000
 	l.mtspr	r0, r13, 0x0a80		#  put in w0tr0
 	
 	# place test value in memory
@@ -73,7 +79,8 @@ _start:
 	l.sw	0x4000(r0), r13		# mem(0x04000) <- 0xbabeface
 		
 	# put pte in page table
-	l.ori	r13, r0, 0x04000001
+	l.movhi	r13, 0x0400
+	l.ori	r13, r13, 0x0001
 	l.sw	0x2000(r0), r13		# pt[0] = 0x0400 0001
 	
 	# activate mmu
